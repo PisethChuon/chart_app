7e359b4ba485fc191c497f545c3694038ffc6a3b
Choun Piseth <chounpiseth@nagaworld.com>
2024-08-20 14:24:18 +0700

    chat state

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -13,16 +14,19 @@ class ChatScreen extends StatelessWidget {
           // Logout Buttom
           IconButton(
             onPressed: () {
-              FirebaseAuth.instance.signOut() ;
+              FirebaseAuth.instance.signOut();
             },
-            icon: Icon(Icons.exit_to_app),
+            icon: const Icon(Icons.exit_to_app),
             color: Theme.of(context).colorScheme.primary,
           ),
         ],
       ),
-      body: Column(
+      body: const Column(
         children: [
-          ChatMessages(),
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessage(),
         ],
       ),
     );
